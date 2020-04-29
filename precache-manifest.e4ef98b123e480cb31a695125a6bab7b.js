39ab6f5d13a9363da0b6c834355b4432b946540e
Valentin <valiopld@gmail.com>
2020-04-30 00:26:42 +0300

    Updates

---
+++ precache-manifest.e4ef98b123e480cb31a695125a6bab7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b71ff60e0fe2e2dd40d570a0968ef35",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b95335352fbb2bae9d43",
+    "url": "/monsters-rolodex/static/css/main.99337b36.chunk.css"
+  },
+  {
+    "revision": "5eed07350c569f900072",
+    "url": "/monsters-rolodex/static/js/2.38ca39a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.38ca39a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b95335352fbb2bae9d43",
+    "url": "/monsters-rolodex/static/js/main.4f6f9ff3.chunk.js"
+  },
+  {
+    "revision": "6575ee065a31e3c1962a",
+    "url": "/monsters-rolodex/static/js/runtime-main.54ff5953.js"
+  }
+]);
